01b31e6ed4b20cfed122e3cdbd979f97099b6fc7
Clare Hyam <clare.hyam@lbi.com>
2012-06-15 17:23:33 +0100

    adding vingette adjustment sliders

--- js/filters.js
+++ js/filters.js
@@ -20,7 +20,12 @@ hipstermatic.filter = {
 				width: 20,
 				color: "white"
 			}
-			//gaussian: true
+		},
+		sepia: {
+			sepia: true
+		},
+		gaussian: {
+			gaussian: true
 		},
 		greyscale: {
 			greyscale: true
@@ -30,7 +35,7 @@ hipstermatic.filter = {
 		}
 	},
 
-	apply:function(config) {
+	apply: function(config) {
 		var canvas = hipstermatic.vars.canvasSelector,
 			imageHolder = hipstermatic.vars.imgObject,
 			canvasWidth = hipstermatic.vars.canvasWidth,
@@ -41,29 +46,9 @@ hipstermatic.filter = {
 			imgPixelsWidth = imgPixels.width;
 
 		if (config.vingette) {
-			//console.log(config.vingette);
-			var outerRadius = Math.sqrt( Math.pow(canvasWidth/2, 2) + Math.pow(canvasHeight/2, 2) );
-            ctx.globalCompositeOperation = 'source-over';
-            //darken corners
-			var grd = ctx.createRadialGradient(canvasWidth/2,canvasHeight/2, 0, canvasWidth/2,canvasHeight/2, outerRadius);
-			grd.addColorStop(0,'rgba(0, 0, 0, 0)');
-			grd.addColorStop(0.5,'rgba(0, 0, 0, 0)');
-			grd.addColorStop(1, 'rgba(0,0,0,' + config.vingette.shadowStrength +')');
-			ctx.fillStyle=grd;
-			ctx.fillRect(0,0,canvasWidth,canvasHeight);
-			//lighten centre
-			ctx.globalCompositeOperation = 'lighter';
-			grd = ctx.createRadialGradient(canvasWidth/2,canvasHeight/2, 0, canvasWidth/2,canvasHeight/2, outerRadius);
-			grd.addColorStop(0,'rgba(255, 255, 255,' + config.vingette.highlightStrength + ')');
-			grd.addColorStop(0.5,'rgba(0, 0, 0, 0)');
-			grd.addColorStop(1, 'rgba(0,0,0, 0)');
-			ctx.fillStyle=grd;
-			ctx.fillRect(0,0,canvasWidth,canvasHeight);
-			ctx.globalCompositeOperation = "source-over"; //setting back to default
-			ctx.fillStyle="#000";
+			this.setVingette(config, ctx, canvasWidth, canvasHeight);
 		}
-		if (config.brightness || config.channelAdjustment) {
-			//try negatives to make darker?
+		if (config.brightness || config.channelAdjustment || config.greyscale || config.sepia) {
 			for (var y = 0; y < imgPixelsHeight; y++) {
 				for (var x = 0; x < imgPixelsWidth; x++) {
 					var i = (y * 4) * imgPixelsWidth + x * 4,
@@ -71,36 +56,45 @@ hipstermatic.filter = {
 					r, g, b;
 					if (config.brightness) {r = config.brightness,g = config.brightness, b = config.brightness;}
 					if (config.channelAdjustment) {r = config.channelAdjustment.red,g = config.channelAdjustment.green, b = config.channelAdjustment.blue;}
-
-					imgPixels.data[i] += r;
-					imgPixels.data[i + 1] += g;
-					imgPixels.data[i + 2] += b;
+					if (config.greyscale){
+						//setPixel
+						//add bluetones etc
+						imgPixels.data[i] = avg;
+						imgPixels.data[i + 1] = avg;
+						imgPixels.data[i + 2] = avg;
+					}
+					else {
+						//adjust pixel
+						imgPixels.data[i] += r;
+						imgPixels.data[i + 1] += g;
+						imgPixels.data[i + 2] += b;
+					}
 				}
 			}
+
 			ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixelsWidth, imgPixelsHeight); // add only one placement for all pixelTweaking
 		}
-		if (config.greyscale) {
-			//greyscale
-			for (var y = 0; y < imgPixelsHeight; y++) {
-				for (var x = 0; x < imgPixelsWidth; x++) {
-					var i = (y * 4) * imgPixelsWidth + x * 4;
-					var avg = (imgPixels.data[i] + imgPixels.data[i + 1] + imgPixels.data[i + 2]) / 3;
-
-					imgPixels.data[i] = avg;
-					imgPixels.data[i + 1] = avg;
-					imgPixels.data[i + 2] = avg;
-				}
-			}
-			ctx.putImageData(imgPixels, 0, 0, 0, 0, imgPixelsWidth, imgPixelsHeight);
+	
+		if (config.gaussian) {
+			this.setGaussian(config, ctx, canvasWidth, canvasHeight, imgPixels);
 		}
-		/*if (config.gaussian) {
+
+		if (config.border){
+			this.setBorder(config, ctx, canvasWidth, canvasHeight);
+		}
+		//canvas.toDataURL(); not sure where to put this yet but seems useful
+
+	},
+	setGaussian: function(config, ctx, canvasWidth, canvasHeight, imageData){
 			function Matrix(){
 				this.rows = [];
 			}
 			//console.log("gaussian");
 			var blurRadius = 7; //temp - move into config
 			var buffer = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
-			
+			var imageDataHeight = imageData.height;
+			var imageDataWidth = imageData.width;
+
 
 			var sumR = 0;
 			var sumG = 0;
@@ -108,36 +102,36 @@ hipstermatic.filter = {
 			var sumA = 0;
 			var gausFact = Array(1,6,15,20,15,6,1);
 			var gausSum = 64;
-			for (var y = 0; y < imgPixelsHeight; y++) {
-				for (var x = 0; x < imgPixelsWidth; x++) {
-				var i = (y * 4) * imgPixelsWidth + x * 4;
-				var sumR = 0;
-				var sumG = 0;
-				var sumB = 0;
-				var sumA = 0;
-				var m = new Matrix();
-				for (var k=0; k<blurRadius; k++) {
+			for (var y = 0; y < imageDataHeight; y++) {
+				for (var x = 0; x < imageDataWidth; x++) {
+					var i = (y * 4) * imageDataWidth + x * 4;
+					sumR = 0;
+					sumG = 0;
+					sumB = 0;
+					sumA = 0;
+					var m = new Matrix();
+					for (var k=0; k<blurRadius; k++) {
 
-					color = ctx.getImageData(parseInt(y+k), x, 1, 1);
-					
-					m.rows[k] = new Array(parseInt(y+k), x);
+						color = ctx.getImageData(parseInt(y+k), x, 1, 1);
+						
+						m.rows[k] = new Array(parseInt(y+k), x);
 
-					var r = color.data[0]; //perhaps merge these if not using elsewhere
-					var g = color.data[1];
-					var b = color.data[2];
-					var a = color.data[3];
+						var r = color.data[0]; //perhaps merge these if not using elsewhere
+						var g = color.data[1];
+						var b = color.data[2];
+						var a = color.data[3];
 
-					sumR += r*gausFact[k];
-					sumG += g*gausFact[k];
-					sumB += b*gausFact[k];
-					sumA += a*gausFact[k]; //might want to take this one out
-					
+						sumR += r*gausFact[k];
+						sumG += g*gausFact[k];
+						sumB += b*gausFact[k];
+						sumA += a*gausFact[k]; //might want to take this one out
+						
 
 
-						//hipstermatic.setPixel(buffer, i, sumR/gausSum, sumG/gausSum, sumB/gausSum, sumA/gausSum);
-					
+							//hipstermatic.setPixel(buffer, i, sumR/gausSum, sumG/gausSum, sumB/gausSum, sumA/gausSum);
+						
 
-				}
+					}
 				//console.log(m);
 				//console.log(m.rows.length);
 			
@@ -150,98 +144,105 @@ hipstermatic.filter = {
 					iData.data[1] = sumG/gausSum;
 					iData.data[2]= sumB/gausSum;
 					iData.data[3] = sumA/gausSum;
-					console.log("x" + xCoord);
-					console.log("y" + yCoord);
+					/*console.log("x" + xCoord);
+					console.log("y" + yCoord);*/
 
-					ctx.putImageData(iData, xCoord, yCoord, 0, 0, imgPixelsWidth, imgPixelsHeight);
+					ctx.putImageData(iData, xCoord, yCoord, 0, 0, imageDataWidth, imageDataHeight);
 
 				}
 			}
 		}
-		}*/
-
-		if (config.border){
-			var borderWidth = config.border.width,
-			borderColor = config.border.color;
-			if (config.border.isRounded){
-				//rounded corners
-				//cornerRadius = { upperLeft: cornerRadius, upperRight: cornerRadius, lowerLeft: cornerRadius, lowerRight: cornerRadius },
-				var radius = config.border.radius,
-				newRectWidth = borderWidth + (canvasWidth - (borderWidth*2)),
-				newRectHeight = borderWidth + (canvasHeight - (borderWidth*2));
-				//composite operation to clip corners
-				ctx.globalCompositeOperation = "destination-in";
-				ctx.save();
-				ctx.beginPath();
-				ctx.lineWidth = borderWidth;
-				//draw rounded rectangle
-				ctx.beginPath();
-				ctx.moveTo(borderWidth + radius, borderWidth);
-				ctx.lineTo(newRectWidth - radius, borderWidth);
-				ctx.quadraticCurveTo(newRectWidth, borderWidth, newRectWidth, borderWidth + radius);
-				ctx.lineTo(newRectWidth, newRectHeight - radius);
-				ctx.quadraticCurveTo(newRectWidth, newRectHeight, newRectWidth - radius, newRectHeight);
-				ctx.lineTo(borderWidth + radius, newRectHeight);
-				ctx.quadraticCurveTo(borderWidth, newRectHeight, borderWidth, newRectHeight - radius);
-				ctx.lineTo(borderWidth, borderWidth + radius);
-				ctx.quadraticCurveTo(borderWidth, borderWidth, borderWidth + radius, borderWidth);
-
-				ctx.closePath();
-				ctx.fill();
-				//set background colour based on color supplied
-				ctx.globalCompositeOperation = "destination-over";
-				ctx.fillStyle = borderColor;
-				ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-				ctx.globalCompositeOperation = "source-over"; //setting back to default
-			}
-		
-			else {
-				//standard corners
-				ctx.beginPath();
-				ctx.rect(0, 0, canvasWidth, canvasHeight);
-				ctx.lineWidth = borderWidth;
-				ctx.strokeStyle = borderColor;
-				ctx.stroke();
-			}
-			
-		}	//canvas.toDataURL(); not sure where to put this yet but seems useful
-
 	},
-	setPixel:function(imageData, index, r, g, b, a){
+	setVingette: function(config, ctx, canvasWidth, canvasHeight){
+		var transparency = 'rgba(0, 0, 0, 0)';
+		//console.log(config.vingette);
+		var outerRadius = Math.sqrt( Math.pow(canvasWidth/2, 2) + Math.pow(canvasHeight/2, 2) );
+        ctx.globalCompositeOperation = 'source-over';
+        //darken corners
+		var grd = ctx.createRadialGradient(canvasWidth/2,canvasHeight/2, 0, canvasWidth/2,canvasHeight/2, outerRadius);
+		grd.addColorStop(0, transparency);
+		grd.addColorStop(0.5, transparency);
+		grd.addColorStop(1, 'rgba(0,0,0,' + config.vingette.shadowStrength +')');
+		ctx.fillStyle=grd;
+		ctx.fillRect(0,0,canvasWidth,canvasHeight);
+		//lighten centre
+		ctx.globalCompositeOperation = 'lighter';
+		grd = ctx.createRadialGradient(canvasWidth/2,canvasHeight/2, 0, canvasWidth/2,canvasHeight/2, outerRadius);
+		grd.addColorStop(0,'rgba(255, 255, 255,' + config.vingette.highlightStrength + ')');
+		grd.addColorStop(0.5, transparency);
+		grd.addColorStop(1, transparency);
+		ctx.fillStyle=grd;
+		ctx.fillRect(0,0,canvasWidth,canvasHeight);
+		ctx.globalCompositeOperation = "source-over"; //setting back to default
+		ctx.fillStyle="#000";
+	},
+	setBorder: function(config, ctx, canvasWidth, canvasHeight){
+
+		var borderWidth = config.border.width,
+		borderColor = config.border.color;
+		if (config.border.isRounded){
+			//rounded corners
+			//cornerRadius = { upperLeft: cornerRadius, upperRight: cornerRadius, lowerLeft: cornerRadius, lowerRight: cornerRadius },
+			var radius = config.border.radius,
+			newRectWidth = borderWidth + (canvasWidth - (borderWidth*2)),
+			newRectHeight = borderWidth + (canvasHeight - (borderWidth*2));
+			//composite operation to clip corners
+			ctx.globalCompositeOperation = "destination-in";
+			ctx.save();
+			ctx.beginPath();
+			ctx.lineWidth = borderWidth;
+			//draw rounded rectangle
+			ctx.beginPath();
+			ctx.moveTo(borderWidth + radius, borderWidth);
+			ctx.lineTo(newRectWidth - radius, borderWidth);
+			ctx.quadraticCurveTo(newRectWidth, borderWidth, newRectWidth, borderWidth + radius);
+			ctx.lineTo(newRectWidth, newRectHeight - radius);
+			ctx.quadraticCurveTo(newRectWidth, newRectHeight, newRectWidth - radius, newRectHeight);
+			ctx.lineTo(borderWidth + radius, newRectHeight);
+			ctx.quadraticCurveTo(borderWidth, newRectHeight, borderWidth, newRectHeight - radius);
+			ctx.lineTo(borderWidth, borderWidth + radius);
+			ctx.quadraticCurveTo(borderWidth, borderWidth, borderWidth + radius, borderWidth);
+
+			ctx.closePath();
+			ctx.fill();
+			//set background colour based on color supplied
+			ctx.globalCompositeOperation = "destination-over";
+			ctx.fillStyle = borderColor;
+			ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+			ctx.globalCompositeOperation = "source-over"; //setting back to default
+		}
 	
-		//set pixel to set colour
-		imageData[index] += r;
-		imageData[index + 1] += g;
-		imageData[index + 2] += b;
-		imageData[index + 3] += a;
-		
+		else {
+			//standard corners
+			ctx.beginPath();
+			ctx.rect(0, 0, canvasWidth, canvasHeight);
+			ctx.lineWidth = borderWidth;
+			ctx.strokeStyle = borderColor;
+			ctx.stroke();
+		}
 	},
 	bindEvents:function(){
-		var canvas = $(hipstermatic.vars.canvasSelector);
-		$(hipstermatic.vars.filterSelector).find("a").bind("click keydown", function(e) {
+		var canvas = $(hipstermatic.vars.canvasSelector),
+		filterLinks = $(hipstermatic.vars.filterSelector).find("a"),
+		vingetteAdjustmentInputs = $(".vingetteAdjustment input"),
+		channelAdjustmentInputs = $(".channelAdjustment input");
+		filterLinks.bind("click keydown", function(e) {
 			// call function to apply the filter
 			if (!e.keyCode || e.keyCode === "13"){
 				var $this = $(this),
 				type = $this.attr("data-filter");
 				if ($this.hasClass("active") || hipstermatic.vars.canvasContext === ""){
-				
 					alert("upload a picture first");
-
-					
 				}
 				else {
 					
-					$(hipstermatic.vars.canvasSelector).trigger("revert");
-
-				
+					canvas.trigger("revert");
 					$this.addClass("active");
-					
 					if (hipstermatic.filter.config[type]) {
 						var canvasUrl = hipstermatic.filter.apply(hipstermatic.filter.config[type]);
-						
 					}
 					else {
-						
+						//no config related to this filter type
 					}
 				}
 				return false;
@@ -249,20 +250,28 @@ hipstermatic.filter = {
 
 		});
 		$("#brightness").bind("change", function(){
-			//console.log($(this).attr("value"));
 			canvas.trigger("revert");
 			hipstermatic.filter.apply({brightness: parseInt($(this).attr("value"), 10)});
 		});
-		$(".channelAdjustment input").bind("change", function(){
-			//console.log($(this).attr("value"));
+		channelAdjustmentInputs.bind("change", function(){
 			canvas.trigger("revert");
-			
 			var t = {channelAdjustment: {}};
-			$(".channelAdjustment input").each(function(){
+			channelAdjustmentInputs.each(function(){
 				var value = parseInt($(this).attr("value"), 10);
 				var id = $(this).attr("id").toString();
 				t.channelAdjustment[id] = value;
-
+			});
+			
+			hipstermatic.filter.apply(t);
+		});
+		vingetteAdjustmentInputs.bind("change", function(){
+			canvas.trigger("revert");
+			var t = {vingette: {}};
+			vingetteAdjustmentInputs.each(function(){
+				//build config to pass through
+				var value = $(this).attr("value");
+				var id = $(this).attr("id").toString();
+				t.vingette[id] = value;
 			});
 			
 			hipstermatic.filter.apply(t);
@@ -276,7 +285,7 @@ hipstermatic.filter = {
 		canvas.bind("revert", function(){
 			//puts back to original image
 			hipstermatic.vars.canvasContext.drawImage(hipstermatic.vars.imgObject, 0, 0, hipstermatic.vars.canvasWidth, hipstermatic.vars.canvasHeight);
-			$(hipstermatic.vars.filterSelector).find("a").removeClass("active");
+			filterLinks.removeClass("active");
 		});
 	},
 		
