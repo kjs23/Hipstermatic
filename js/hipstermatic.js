f23d4794311f6146b15dd5655621bc9019080bab
Jasal Vadgama <jasal@blacklabelcreative.com>
2012-06-20 17:00:42 +0100

    rework of layout

--- js/hipstermatic.js
+++ js/hipstermatic.js
@@ -40,13 +40,13 @@ hipstermatic.loadImageFromFile = function(e) {
 			// clear current canvas content
 			hipstermatic.vars.canvasContext.clearRect(0, 0, $canvas.width(), $canvas.height());
 
-			// scale the image down to a max 500x500
+			// scale the image down to a max 800x500
 			if (imgWidth > imgHeight) {
 				// calculate the scale factor
-				imgScaleFactor = imgWidth / 500;
+				imgScaleFactor = imgWidth / 800;
 
 				// set the new width and height
-				hipstermatic.vars.canvasWidth = 500;
+				hipstermatic.vars.canvasWidth = 800;
 				hipstermatic.vars.canvasHeight = parseInt((imgHeight / imgScaleFactor), 10);//parseInt to stop odd numbers in chrome
 			} else {
 				// calculate the scale factor
@@ -61,33 +61,76 @@ hipstermatic.loadImageFromFile = function(e) {
 			$canvas.attr({
 				width: hipstermatic.vars.canvasWidth,
 				height: hipstermatic.vars.canvasHeight
+			}).css({
+				marginLeft: -($canvas.width() / 2)
 			});
 
 			// draw image on canvas
 			// ctx.drawImage([image], [top], [left], [width], [height]);
 			hipstermatic.vars.canvasContext.drawImage(hipstermatic.vars.imgObject, 0, 0, hipstermatic.vars.canvasWidth, hipstermatic.vars.canvasHeight);
 
-			$('.filters').slideDown(1000);
+			// show the filters down
+			$('#sidebar').find('.togglePanel').animate({
+				left: -92
+			});
+
+			// unbind the canvas click and show the 'change' button
+			$('canvas').off('click');
 		};
 	};
 
 	reader.readAsDataURL(e.target.files[0]);
 };
 
-hipstermatic.saveImage = function() {
-	// save the image
+hipstermatic.initSidebar = function() {
+	var $sidebar = $('#sidebar');
+
+	// set init position
+	$sidebar.css({
+		//marginTop: -($sidebar.height() / 2),
+		visibility: 'visible',
+		height: $(window).height() - 20
+	}).animate({
+		right: -292
+	}, 0);
+
+	// bind toggle to h2
+	$sidebar.find('.togglePanel').on('click', function(e) {
+		var animateTo;
+
+		e.preventDefault();
+
+		if ($sidebar.hasClass('active')) {
+			animateTo = -292;
+		} else {
+			animateTo = -1;
+		}
+
+		$sidebar.stop(true, true).animate({
+			right: animateTo
+		}).toggleClass('active');
+	});
 };
 
 hipstermatic.init = function() {
 	var $input = $('#input');
 
 	// bind file input
-	$input.bind('change', function(e) {
+	$input.on('change', function(e) {
 		hipstermatic.loadImageFromFile(e);
 	});
 
-	// roll up the filters
-	$('.filters').slideUp(0);
+	// bind save button
+	$('.save').on('click', function() {
+		hipstermatic.storage.saveImage();
+	});
+
+	$('canvas').on('click', function() {
+		// click the hidden form
+		$('#input').click();
+	});
+
+	hipstermatic.initSidebar();
 };
 		
 $(function() {
---
+++ js/storage.js
@@ -0,0 +1,96 @@
+/**
+* Hipstermatic - Arty <canvas> photos filters
+*
+* @info		localStorage functions
+* @version	0.1
+* @author	Kevin Stevens, Clare Hyam, Chloe Watts, Jasal Vadgama
+* @require	jquery 1.7.1+
+* @license	GPL v3
+**/
+
+var hipstermatic = hipstermatic || {};
+
+hipstermatic.storage = {
+	/* Storage vars */
+	config: {
+		imageDataInStorage: '', // raw string image data held in local storage
+		imageJsonInStorage: {} // parsed JSON of the image data in local storage
+	},
+
+	saveImage: function() {
+		// get data URL (base64 encoded) and add to local storage
+		var canvas = document.getElementById(hipstermatic.vars.canvasSelector.split('#')[1]);
+			dataURL = canvas.toDataURL(),
+			imageTitle = $('#imageTitle').val();
+
+		// if image title is empty
+		if (!imageTitle) {
+			// show error msg
+			alert('Please enter an image name.');
+			return;
+		}
+
+		// make sure name doesn't already exist
+		for (i = 0; i < hipstermatic.storage.config.imageJsonInStorage.images.length; i++) {
+			if (hipstermatic.storage.config.imageJsonInStorage.images[i].title === imageTitle) {
+				// name exists so show error and quit
+				alert('That name already exists, please chose another.');
+				return;
+			}
+		}
+
+		// push data to json object
+		hipstermatic.storage.config.imageJsonInStorage.images.push({
+			title: imageTitle,
+			data: dataURL
+		});
+		//console.log(hipstermatic.storage.config.imageJsonInStorage);
+
+		// stringify json
+		hipstermatic.storage.config.imageDataInStorage = JSON.stringify(hipstermatic.storage.config.imageJsonInStorage);
+		//console.log(hipstermatic.storage.config.imageDataInStorage);
+
+		// add to local storage
+		localStorage.setItem('hipstermatic.images', hipstermatic.storage.config.imageDataInStorage);
+
+		alert('"' + imageTitle + '" has been saved');
+	},
+
+	deleteImage: function() {
+		// delete a saved image
+	},
+
+	getImageList: function() {
+		// get list of saved images
+		var $ul = $('<ul class="imageList" />'),
+			savedImages = hipstermatic.storage.config.imageJsonInStorage.images;
+
+		for (i = 0; i < savedImages.length; i++) {
+			//console.log(savedImages[i].title);
+
+			$('<li><img src="' + savedImages[i].data + '" /> ' + savedImages[i].title + '</li>').appendTo($ul);
+		}
+	},
+
+	viewImage: function() {
+		// view selected image
+	},
+
+	init: function() {
+		// get image items from local storage
+		// if nothing there set to empty string
+		hipstermatic.storage.config.imageDataInStorage = localStorage['hipstermatic.images'] || '';
+
+		// convert raw image data to JSON
+		// if nothing there set up new object
+		hipstermatic.storage.config.imageJsonInStorage = $.parseJSON(hipstermatic.storage.config.imageDataInStorage) || { "images": [] };
+
+		//console.log(localStorage['hipstermatic.images']);
+
+		hipstermatic.storage.getImageList();
+	}
+};
+
+$(function() {
+	hipstermatic.storage.init();
+});
