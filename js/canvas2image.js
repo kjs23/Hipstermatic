153bbbc23e76e0158cdd6c25ce3e8537d9a8a62c
chloedigital <info@chloedigital.com>
2012-07-02 11:08:44 +0100

    cleaned out unnecessary script
    cleaned out unnecessary script

--- js/canvas2image.js
+++ js/canvas2image.js
@@ -5,10 +5,9 @@
  */
 
 var Canvas2Image = (function() {
-
+	var oCanvas = document.getElementById("myCanvas");
 	// check if we have canvas support
 	var bHasCanvas = false;
-	var oCanvas = document.createElement("canvas");
 	if (oCanvas.getContext("2d")) {
 		bHasCanvas = true;
 	}
@@ -16,8 +15,6 @@ var Canvas2Image = (function() {
 	// no canvas, bail out.
 	if (!bHasCanvas) {
 		return {
-			saveAsBMP : function(){},
-			saveAsPNG : function(){},
 			saveAsJPEG : function(){}
 		}
 	}
@@ -48,100 +45,6 @@ var Canvas2Image = (function() {
 		}
 		return btoa(strData);
 	}
-
-	// creates a base64 encoded string containing BMP data
-	// takes an imagedata object as argument
-	var createBMP = function(oData) {
-		var aHeader = [];
-	
-		var iWidth = oData.width;
-		var iHeight = oData.height;
-
-		aHeader.push(0x42); // magic 1
-		aHeader.push(0x4D); 
-	
-		var iFileSize = iWidth*iHeight*3 + 54; // total header size = 54 bytes
-		aHeader.push(iFileSize % 256); iFileSize = Math.floor(iFileSize / 256);
-		aHeader.push(iFileSize % 256); iFileSize = Math.floor(iFileSize / 256);
-		aHeader.push(iFileSize % 256); iFileSize = Math.floor(iFileSize / 256);
-		aHeader.push(iFileSize % 256);
-
-		aHeader.push(0); // reserved
-		aHeader.push(0);
-		aHeader.push(0); // reserved
-		aHeader.push(0);
-
-		aHeader.push(54); // dataoffset
-		aHeader.push(0);
-		aHeader.push(0);
-		aHeader.push(0);
-
-		var aInfoHeader = [];
-		aInfoHeader.push(40); // info header size
-		aInfoHeader.push(0);
-		aInfoHeader.push(0);
-		aInfoHeader.push(0);
-
-		var iImageWidth = iWidth;
-		aInfoHeader.push(iImageWidth % 256); iImageWidth = Math.floor(iImageWidth / 256);
-		aInfoHeader.push(iImageWidth % 256); iImageWidth = Math.floor(iImageWidth / 256);
-		aInfoHeader.push(iImageWidth % 256); iImageWidth = Math.floor(iImageWidth / 256);
-		aInfoHeader.push(iImageWidth % 256);
-	
-		var iImageHeight = iHeight;
-		aInfoHeader.push(iImageHeight % 256); iImageHeight = Math.floor(iImageHeight / 256);
-		aInfoHeader.push(iImageHeight % 256); iImageHeight = Math.floor(iImageHeight / 256);
-		aInfoHeader.push(iImageHeight % 256); iImageHeight = Math.floor(iImageHeight / 256);
-		aInfoHeader.push(iImageHeight % 256);
-	
-		aInfoHeader.push(1); // num of planes
-		aInfoHeader.push(0);
-	
-		aInfoHeader.push(24); // num of bits per pixel
-		aInfoHeader.push(0);
-	
-		aInfoHeader.push(0); // compression = none
-		aInfoHeader.push(0);
-		aInfoHeader.push(0);
-		aInfoHeader.push(0);
-	
-		var iDataSize = iWidth*iHeight*3; 
-		aInfoHeader.push(iDataSize % 256); iDataSize = Math.floor(iDataSize / 256);
-		aInfoHeader.push(iDataSize % 256); iDataSize = Math.floor(iDataSize / 256);
-		aInfoHeader.push(iDataSize % 256); iDataSize = Math.floor(iDataSize / 256);
-		aInfoHeader.push(iDataSize % 256); 
-	
-		for (var i=0;i<16;i++) {
-			aInfoHeader.push(0);	// these bytes not used
-		}
-	
-		var iPadding = (4 - ((iWidth * 3) % 4)) % 4;
-
-		var aImgData = oData.data;
-
-		var strPixelData = "";
-		var y = iHeight;
-		do {
-			var iOffsetY = iWidth*(y-1)*4;
-			var strPixelRow = "";
-			for (var x=0;x<iWidth;x++) {
-				var iOffsetX = 4*x;
-
-				strPixelRow += String.fromCharCode(aImgData[iOffsetY+iOffsetX+2]);
-				strPixelRow += String.fromCharCode(aImgData[iOffsetY+iOffsetX+1]);
-				strPixelRow += String.fromCharCode(aImgData[iOffsetY+iOffsetX]);
-			}
-			for (var c=0;c<iPadding;c++) {
-				strPixelRow += String.fromCharCode(0);
-			}
-			strPixelData += strPixelRow;
-		} while (--y);
-
-		var strEncoded = encodeData(aHeader.concat(aInfoHeader)) + encodeData(strPixelData);
-
-		return strEncoded;
-	}
-
 
 	// sends the generated file to the client
 	var saveFile = function(strData) {
@@ -176,21 +79,6 @@ var Canvas2Image = (function() {
 	}
 
 	return {
-
-		saveAsPNG : function(oCanvas, bReturnImg, iWidth, iHeight) {
-			if (!bHasDataURL) {
-				return false;
-			}
-			var oScaledCanvas = scaleCanvas(oCanvas, iWidth, iHeight);
-			var strData = oScaledCanvas.toDataURL("image/png");
-			if (bReturnImg) {
-				return makeImageObject(strData);
-			} else {
-				saveFile(strData.replace("image/png", strDownloadMime));
-			}
-			return true;
-		},
-
 		saveAsJPEG : function(oCanvas, bReturnImg, iWidth, iHeight) {
 			if (!bHasDataURL) {
 				return false;
@@ -212,24 +100,40 @@ var Canvas2Image = (function() {
 				saveFile(strData.replace(strMime, strDownloadMime));
 			}
 			return true;
-		},
-
-		saveAsBMP : function(oCanvas, bReturnImg, iWidth, iHeight) {
-			if (!(bHasImageData && bHasBase64)) {
-				return false;
-			}
-
-			var oScaledCanvas = scaleCanvas(oCanvas, iWidth, iHeight);
-
-			var oData = readCanvasData(oScaledCanvas);
-			var strImgData = createBMP(oData);
-			if (bReturnImg) {
-				return makeImageObject(makeDataURI(strImgData, "image/bmp"));
-			} else {
-				saveFile(makeDataURI(strImgData, strDownloadMime));
-			}
-			return true;
 		}
+
 	};
 
 })();
+
+window.onload = function() {
+  	var oCanvas = document.getElementById("myCanvas");
+
+		// function convertCanvas(strType) {
+			
+		// 	if (strType == "JPEG")
+		// 	  var oImg = Canvas2Image.saveAsJPEG(oCanvas, true);
+
+		// 	if (!oImg) {
+		// 	  alert("Sorry, this browser is not capable of saving " + strType + " files!");
+		// 	  return false;
+		// 	}
+
+		// 	oImg.id = "canvasimage";
+
+		// 	oImg.style.border = oCanvas.style.border;
+		// 	oCanvas.parentNode.replaceChild(oImg, oCanvas);
+
+		// 	showDownloadText();
+		// }
+		function saveCanvas(pCanvas, strType) {
+			 Canvas2Image.saveAsJPEG(oCanvas);
+
+  		}
+  		//document.getElementById("convertjpegbtn").onclick = function() {
+		// 	convertCanvas("JPEG");
+		// }
+		document.getElementById("saveToDisk").onclick = function() {
+			saveCanvas(oCanvas, "JPEG");
+		}
+	}
